9795cc0fa41a84d8ceb6973d0913e66427fd0964
Diego Braun <diegobraun2000@gmail.com>
2019-02-21 10:42:41 -0300

    Addition of simple validation

---
+++ MarketSystem/MarketSystem/ClientFeatures/Validation/ValidationClient.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MarketSystem_Client_Client;
+
+namespace MarketSystem.ClientFeatures.Validation
+{
+    class ValidationClient
+    {
+        public bool CheckIfIsNotEmpty (string content)
+        {
+            if (string.IsNullOrWhiteSpace(content))
+                return true;
+            else
+                return false;
+        }
+
+        public bool CheckIfIsValidClient (Client client)
+        {
+            
+        }
+    }
+}
