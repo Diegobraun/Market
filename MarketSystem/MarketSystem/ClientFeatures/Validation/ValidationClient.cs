c7b65dcaa5f8a6bc5ff9e2a90645eedd74552299
Diego Braun <diegobraun2000@gmail.com>
2019-02-22 10:39:03 -0300

    Addition of validation

--- MarketSystem/MarketSystem/ClientFeatures/Validation/ValidationClient.cs
+++ MarketSystem/MarketSystem/ClientFeatures/Validation/ValidationClient.cs
@@ -16,10 +16,32 @@ namespace MarketSystem.ClientFeatures.Validation
             else
                 return false;
         }
-
+        
         public bool CheckIfIsValidClient (Client client)
         {
-            return true;
+            bool check = true;
+            List<object> values = new List<object>();
+            values.Add(client.FirstName);
+            values.Add(client.LastName);
+            values.Add(client.Email);
+            values.Add(client.Gender);
+            values.Add(client.DateOfBirth);
+            values.Add(client.Cpf);
+            values.Add(client.Password);
+            values.Add(client.Contact.Type);
+            values.Add(client.Contact.Description);
+            values.Add(client.Address.Type);
+            values.Add(client.Address.Street);
+            values.Add(client.Address.Number);
+            values.Add(client.Address.District);
+            values.Add(client.Address.City);
+            foreach(object value in values)
+            {
+                if (CheckIfIsNotEmpty(value.ToString()))
+                    check = false;
+            }
+
+            return check;
         }
     }
 }
