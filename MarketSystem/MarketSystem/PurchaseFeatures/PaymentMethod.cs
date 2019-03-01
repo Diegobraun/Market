f659a84f1fe589305cd3a883e698272d883c1d77
Diego Braun <diegobraun@localhost.localdomain>
2019-03-01 01:11:39 -0300

    Removing .net

--- MarketSystem/MarketSystem/PurchaseFeatures/PaymentMethod.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MarketSystem_Purchase_PaymentMethod
-{
-    public class PaymentMethod
-    {
-        public int PaymentMethodId { get; private set; }
-        public int Type { get; private set; }
-
-    }
-}
